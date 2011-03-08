bfb12d8079bc54217bdb28d3155301d3c2afddc6
Finn Thain <fthain@telegraphics.com.au>
2011-03-08 12:03:02 +1000

    find.pl v0.17

--- find.pl
+++ find.pl
@@ -1,7 +1,8 @@
 #!/usr/bin/perl
 
 # find(1) for perl programmers.
-# Copyright (c) 2006 Finn Thain
+
+# Copyright (c) 2006-2011 Finn Thain
 # fthain@telegraphics.com.au
 
 # Permission is hereby granted, free of charge, to any person obtaining a copy
@@ -10,10 +11,10 @@
 # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 # copies of the Software, and to permit persons to whom the Software is
 # furnished to do so, subject to the following conditions:
- 
+
 # The above copyright notice and this permission notice shall be included in all
 # copies or substantial portions of the Software.
- 
+
 # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -25,27 +26,32 @@
 
 use strict;
 use warnings;
-use POSIX qw(strftime S_ISREG S_ISDIR);
+use POSIX qw(strftime
+             S_IRGRP S_IROTH S_IRUSR S_IRWXG S_IRWXO S_IRWXU
+             S_ISBLK S_ISCHR S_ISDIR S_ISFIFO S_ISGID S_ISREG S_ISUID
+             S_IWGRP S_IWOTH S_IWUSR);
 use Digest::MD5;
 
 use Getopt::Std;
 $Getopt::Std::STANDARD_HELP_VERSION = 1;
-$::VERSION = '0.12';
+$::VERSION = '0.17';
 sub HELP_MESSAGE() {
   print "Usage: $0 [-0] [-d] [-e <perl>] [-h] [-n] [-s] [-t <perl>] [-v] [-x] pathname...\n";
-  print 'Options: -0 Use a null to seperate pathnames listed in output.
+  print 'Options:
+         -- Treat all remaining arguments as pathnames.
+         -0 Use a null to seperate pathnames listed in output.
          -d Descend directory tree depth first.
          -e Execute perl snippet for every directory entry. $_ is set to the
-	    pathname to make the unary file test operators more convenient. The
-	    basename and dirname are in $base and $dir, search depth is in
-	    $depth and stat() results are available in $dev, $ino, $mode,
-	    $nlink, $uid, $gid, $rdev, $size, $atime, $mtime, $ctime, $blksize
-	    and $blocks. You can call prune() to prevent descent into the
-	    current directory (except with -d), md5() to get the hex MD5 digest
-	    of the current file and ls() if you want to list the current entry
-	    (called by default if you return true).
+            pathname to make the unary file test operators more convenient. The
+            basename and dirname are in $base and $dir, search depth is in
+            $depth and stat() results are available in $dev, $ino, $mode,
+            $nlink, $uid, $gid, $rdev, $size, $atime, $mtime, $ctime, $blksize
+            and $blocks. You can call prune() to prevent descent into the
+            current directory (except with -d), md5() to get the hex MD5 digest
+            of the current file and ls() if you want to list the current entry
+            (called by default if you return true).
          -h Follow symlinks.
-         -n Show GID and UID numerically (implies -v).
+         -n Show GID and UID numerically (with -v).
          -s Process directory entries in ascending order of basename.
          -t Execute perl snippet before terminating.
          -v Verbose listing, like tar(1).
@@ -61,7 +67,7 @@ our ( $dev, $ino, $mode, $nlink, $uid, $gid, $rdev, $size,
 my %hlinks;
 my $width = 19;
 sub ls {
-  if( $opt_v or $opt_n ) {
+  if( $opt_v ) {
     local $_ = $_;
     local $size = $size;
     local $uid = $uid;
@@ -80,7 +86,7 @@ sub ls {
     if ( !$tmode ) {
       if( -l and !$opt_h ) {
         if( !defined( $target = readlink $_ ) ) {
-          print STDERR "$0: $_: readlink failed: $!\n";
+          print STDERR "$0: readlink: $!: $_\n";
           return
         }
         $tmode = 'l';
@@ -110,7 +116,7 @@ sub ls {
         }
       }
     }
-    
+
     my $xsuid  = 'x';
     my $xsgid  = 'x';
     my $sticky = 'x';
@@ -121,7 +127,7 @@ sub ls {
     }
     my $suid   = (-u) ? 'S' : '-';
     my $sgid   = (-g) ? 'S' : '-';
-  
+
     $tmode = $tmode.
            ($mode&0400?'r':'-').($mode&0200?'w':'-').($mode&0100?$xsuid:$suid).
            ($mode&0040?'r':'-').($mode&0020?'w':'-').($mode&0010?$xsgid:$sgid).
@@ -159,9 +165,11 @@ sub ls {
 
 
 sub md5 {
+  my $fn = shift;
+  $fn = $_ if !defined $fn;
   local *FH;
-  open( FH, "<$_" ) or die 'open failed\n';
-  binmode( FH );
+  open( FH, "<$fn" ) or die "open failed\n";
+  binmode FH;
   my $result = Digest::MD5->new->addfile( *FH )->hexdigest;
   close FH;
   $result
@@ -184,12 +192,9 @@ sub process {
     ( $dev, $ino, $mode, $nlink, $uid, $gid, $rdev, $size,
       $atime, $mtime, $ctime, $blksize, $blocks ) = ( stat )[ 0..12 ];
   }
-  if( !defined $blocks ) {
-    if( -l ) {
-      print STDERR "$0: $_: (l)stat failed: $!\n" 
-    } else {
-      die "$0: $_: (l)stat failed: $!\n" 
-    }
+  if( !defined $dev ) {
+    print STDERR "$0: (l)stat: $!: $_\n";
+    return 1
   }
 
   if( defined $parent_dev ) {
@@ -202,26 +207,28 @@ sub process {
   if( defined $opt_e ) {
     local $_ = $_;
     $ok = eval $opt_e;
-    die "$0: $_: eval failed: $@" if $@ ne ''
+    die "$0: eval: $@: $_" if $@ ne ''
   }
-  ls if $ok
+  ls if $ok;
+  return 0
 }
 
 
 sub descend {
+  my $result;
   my ( $dir, $base ) = @_;
   local $_ = $dir.$base;
 
   if( !$opt_d ) {
     $pruned = 0;
-    process( $dir, $base );
-    return if $pruned;
+    $result = process( $dir, $base );
+    return $result if $pruned;
   }
 
   if( -d and ( $opt_h or !-l ) ) {
     $depth++;
     local *D;
-    opendir( D, $_ ) or print STDERR "$0: $_: opendir failed: $!\n";
+    opendir( D, $_ ) or print STDERR "$0: opendir: $!: $_\n";
     my @ents = readdir D;
     closedir D;
     foreach my $ent ( $opt_s ? sort @ents : @ents ) {
@@ -231,18 +238,24 @@ sub descend {
     $depth--;
   }
 
-  process( $dir, $base ) if $opt_d
+  $result = process( $dir, $base ) if $opt_d;
+  return $result
 }
 
+
+my $exit_status = 0;
 
 my $arg;
 foreach $arg ( @ARGV ) {
   $arg =~ m,(.*/)?([^/]*)$,;
   $depth = 0;
-  descend( defined $1 ? $1 : '', $2 )
+  $parent_dev = undef;
+  $exit_status = descend( defined $1 ? $1 : '', $2 ) || $exit_status
 }
 
 if( defined $opt_t ) {
   eval $opt_t;
-  die "$0: eval failed: $@" if $@ ne ''
+  die "$0: END eval: $@" if $@ ne ''
 }
+
+exit $exit_status
